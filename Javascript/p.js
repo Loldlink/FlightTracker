775cd5246ccc729c8e0832ef0a67f977bbbc36ca
Loldlink <62999996+Loldlink@users.noreply.github.com>
2021-04-12 21:13:14 +0200

    Update p.js

--- Javascript/p.js
+++ Javascript/p.js
@@ -5,8 +5,8 @@ function httpGet() {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open("GET", theUrl, false); 
     xmlHttp.send(null);
-    var info = xmlHttp.responseText;
-    const j = JSON.parse(info);
+    var info = xmlHttp.responseText;    //all'interno di info hai il json con tutti i dati di vatsim
+    const j = JSON.parse(info);         //in j hai un dizionario chiave/valore 
     var pippo = j.pilots[0];
 }
     
