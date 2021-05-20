6b005ee7ee0e3aee08c7ba051d01d7805e494900
Lorenzo <magni.1872113@studenti.uniroma1.it>
2021-05-20 17:38:53 +0200

    Correzione sfondi + About us
    Corretti alcuni sfondi e ritoccata un po' l'about us.

--- Javascript/cambiaSfondo3.js
+++ Javascript/cambiaSfondo3.js
@@ -1,11 +1,10 @@
 //i link sono del mio facebook personale e sono accessibili a tutti
 
 function sfondo() {
-    var urls= ["https://scontent.ffco3-1.fna.fbcdn.net/v/t1.6435-9/186511931_4027798077298394_8687160041061994482_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=0debeb&_nc_ohc=YCZFWkOudTgAX-QXi6a&_nc_ht=scontent.ffco3-1.fna&oh=35fdfda2a7720ac81c78099c811202fa&oe=60C8401A",
-    "https://scontent.ffco3-1.fna.fbcdn.net/v/t1.6435-9/186513774_4027798010631734_5040818546402806452_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=0debeb&_nc_ohc=75VYlkwbGboAX-9hTkM&_nc_ht=scontent.ffco3-1.fna&oh=4e3bde9a8c78ca83e3cbe6139ae2ea7e&oe=60C6F00B",
-    "https://scontent.ffco3-1.fna.fbcdn.net/v/t1.6435-9/186544552_4027798413965027_5824534003111505708_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=0debeb&_nc_ohc=3Azlt2IuFRgAX_HIk36&_nc_ht=scontent.ffco3-1.fna&oh=7e8cfad5cb90b11c9c2b1785f3eafeed&oe=60C878BD",
-    "https://scontent.ffco3-1.fna.fbcdn.net/v/t1.6435-9/187172669_4027798380631697_1192086749001485309_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=0debeb&_nc_ohc=5vOPCD7tYX4AX8Oh19c&_nc_ht=scontent.ffco3-1.fna&oh=bc6d210614923c2ffffbf3a733f55b8b&oe=60C6C5FC",
-    "https://scontent.ffco3-1.fna.fbcdn.net/v/t1.6435-9/187414939_4027797917298410_7754313945169935309_n.jpg?_nc_cat=109&ccb=1-3&_nc_sid=0debeb&_nc_ohc=CzChBGl04aoAX-ejEus&_nc_ht=scontent.ffco3-1.fna&oh=562bc7f792955a0333806b5ba720673f&oe=60C6923A",
+    var urls= ["https://scontent.ffco3-1.fna.fbcdn.net/v/t1.6435-9/188745757_4038080419603493_7129253532541067755_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=0debeb&_nc_ohc=YszKNK_wu4UAX8WpRM8&_nc_ht=scontent.ffco3-1.fna&oh=1eeb83b23082c9aa0d5594487c0ad4ba&oe=60CB1911",
+    "https://scontent.ffco3-1.fna.fbcdn.net/v/t1.6435-9/188826511_4038080406270161_4531373592401115788_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=0debeb&_nc_ohc=FYFEEAlB_5AAX-14mIw&_nc_ht=scontent.ffco3-1.fna&oh=d0dfc9943fae286ea87fb40b594caeaa&oe=60CACF55",
+    "https://scontent.ffco3-1.fna.fbcdn.net/v/t1.6435-9/187735002_4038080402936828_940062446066398320_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=0debeb&_nc_ohc=LkvYA5O5MA0AX_B0NyL&_nc_ht=scontent.ffco3-1.fna&oh=8dcddafacb985769cab5a39ffb43034c&oe=60CA818C",
+    "https://scontent.ffco3-1.fna.fbcdn.net/v/t1.6435-9/186957610_4027798117298390_6402413150813070104_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=0debeb&_nc_ohc=Fg1s0I-pNYgAX941O77&_nc_ht=scontent.ffco3-1.fna&oh=49651b4109cc460d4920a295c187af97&oe=60CA35B3",
     "https://scontent.ffco3-1.fna.fbcdn.net/v/t1.6435-9/188074502_4027798147298387_6981127181761857889_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=0debeb&_nc_ohc=K2BcklKCB6sAX_zoGtf&_nc_ht=scontent.ffco3-1.fna&oh=1978389c8259026847979a68870fbe28&oe=60C8361B",
     ];
 
